af4cb173ef54aed01954b482b72c5269a07c1d93
ZEBRAsarasa <xiaotianyouping287@gmail.com>
2021-11-07 14:22:14 +0000

    [Finish] 4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '673b3b1dc92b2068f1d1d3a4afc5980ca151874a9112b79787cdc3d1d64aa472b197bda58f16c1a52ef72264da1428d0f5a8487543225263d62aad0bd86fea7e'
